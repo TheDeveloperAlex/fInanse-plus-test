c96a0271deff50b0eaba845d82b7d5f9e3993cef
TheDeveloperAlex <scorpion422@ukr.net>
2026-09-15 22:02:05 +0200

    feat: wrap TemplateStudio in HydrationGate

--- components/studio/TemplateStudio.tsx
+++ components/studio/TemplateStudio.tsx
@@ -1,11 +1,14 @@
+import { HydrationGate } from './HydrationGate';
 import { StudioBody } from './StudioBody';
 import { TopBar } from './TopBar';
 
 export function TemplateStudio() {
   return (
     <div className="flex h-screen flex-col bg-canvas text-ink">
-      <TopBar />
-      <StudioBody />
+      <HydrationGate>
+        <TopBar />
+        <StudioBody />
+      </HydrationGate>
     </div>
   );
 }
