58f87ef812648543e2577f00f4b54286574c977e
TheDeveloperAlex <scorpion422@ukr.net>
2026-09-15 15:16:06 +0200

    chore: mock ResizeObserver for Radix UI testing

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,6 +2,14 @@ import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 
+// Mock ResizeObserver for Radix UI components in jsdom
+global.ResizeObserver = class ResizeObserver {
+  constructor() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
 afterEach(() => {
   cleanup();
 });
